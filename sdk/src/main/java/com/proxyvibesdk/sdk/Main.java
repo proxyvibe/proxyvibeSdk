51534ef0f1b988697b0db9812834896d00da7334
ffucucuoglu <fuatfucucuoglu@outlook.com>
2023-09-03 11:17:55 +0400

    init

---
+++ sdk/src/main/java/com/proxyvibesdk/sdk/Main.java
@@ -0,0 +1,12 @@
+package com.proxyvibesdk.sdk;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class Main {
+    public static void s(Context c, String message){
+
+        Toast.makeText(c,message, Toast.LENGTH_SHORT).show();
+
+    }
+}
